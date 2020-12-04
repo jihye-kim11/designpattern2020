c40cb7f5a28cbc172b27adca1951b03569f7d4a4
jihye-kim11 <59490892+jihye-kim11@users.noreply.github.com>
2020-12-04 22:37:14 +0900

    xml importer

--- DP2020Project/DP2020Project/src/com/holub/database/DP2020Project/src/com/holub/database/XmlImporter.java
+++ DP2020Project/DP2020Project/src/com/holub/database/DP2020Project/src/com/holub/database/XmlImporter.java
@@ -33,8 +33,9 @@ import java.util.*;
 
 
 public class XmlImporter implements Table.Importer
-{	private BufferedReader  in;			// null once end-of-file reached
-	private String[]        columnNames;
+{	private BufferedReader  in;			// 파일의 끝에 도달하면 null
+	private ArrayList<String>       columnNames;//일반적으로 원소에 랜덤으로 접근할 수 있어야 하거나 리스트 크기가 클수록 ArrayList를 사용하면 좋다.
+	private LinkedList<String>       D;//리스트의 첫 부분이나 중간에 원소를 삽입/삭제할 일들이 많다면 LinkedList를 사용하면 좋다.
 	private String          tableName;
 
 	public XmlImporter( Reader in )
@@ -44,30 +45,92 @@ public class XmlImporter implements Table.Importer
 	                    ;
 	}
 	public void startTable()			throws IOException
-	{	tableName   = in.readLine().trim();
-		columnNames = in.readLine().split("\\s*,\\s*");
+	{
+		String firstline=in.readLine().trim();
+	    String header="<?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?>";
+	
+	    if(firstline.equals(header)) {
+		String tablen=in.readLine().trim();
+		tableName=tablen.substring(1,tablen.length()-1);//테이블네임 가져오기
+		
+		String a;
+		if((a=in.readLine().trim()).equals("<row>")) {
+			while(!(a=in.readLine().trim()).equals("</row>")) {
+				String data="";
+				for(int i=1; i<a.length(); i++) {
+					if(a.charAt(i)=='>') {//columnName 완료된것
+						D.offer(a.substring(i+1));//Queue에 삽입
+						break;
+					}
+					else {
+						data+=a.charAt(i);
+					}
+				}
+				columnNames.add(data);//데이터 columnName 추가
+			}
+		}
+	    }
+	    else {System.out.println("잘못된 파일입니다.\n");
+	    }
 	}
 	public String loadTableName()		throws IOException
 	{	return tableName;
 	}
 	public int loadWidth()			    throws IOException
-	{	return columnNames.length;
+	{	return columnNames.size();
 	}
 	public Iterator loadColumnNames()	throws IOException
-	{	return new ArrayIterator(columnNames);  //{=CSVImporter.ArrayIteratorCall}
+	{	return new ArrayIterator(columnNames.toArray()); 
 	}
 
 	public Iterator loadRow()			throws IOException
-	{	Iterator row = null;
-		if( in != null )
-		{	String line = in.readLine();
-			if( line == null )
-				in = null;
-			else
-				row = new ArrayIterator( line.split("\\s*,\\s*"));
-		}
-		return row;
+	{	 String[] row = new String[columnNames.size()];
+		
+	 if (!D.isEmpty()) {
+		 while(!D.isEmpty()) {
+			 for(int i=0; i<row.length; i++) {
+				 String a=D.poll();//Queue에서 제거하며 읽기
+				 int end=a.indexOf("<");
+				 String data=a.substring(0, end);
+				 row[i]=data;
+				
+			 }
+		 }
+	 }
+	 else {
+		 String a= in.readLine().trim();
+		 if(a.equals("<row>")) {
+			 int i=0;
+			 while(!(a=in.readLine().trim()).equals("</row>")) {
+				 int start=a.indexOf(">")+1;
+				 int end=a.indexOf("<");
+				 String data=a.substring(start, end);
+				 row[i]=data;
+				 i+=1;
+			 }
+		 }
+		 else {return null;}
+	 }
+	 return new ArrayIterator( row );
 	}
 
 	public void endTable() throws IOException {}
+	
+public static class Test{
+	public static void main( String[] args ) throws IOException
+	{	
+		Table people = TableFactory.create( "people",
+					   new String[]{ "First", "Last"		} );
+		people.insert( new String[]{ "Allen",	"Holub" 	} );
+		people.insert( new String[]{ "Ichabod",	"Crane" 	} );
+		people.insert( new String[]{ "Rip",		"VanWinkle" } );
+		people.insert( new String[]{ "Goldie",	"Locks" 	} );
+
+		
+		 Writer writer = new FileWriter("C://dp2020");
+		 people.export(new XmlExporter(writer));
+            writer.close();
+
+	}
+	}
 }
